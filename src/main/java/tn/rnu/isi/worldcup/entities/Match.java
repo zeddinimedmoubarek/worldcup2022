4a9bfa4f7847bf9f5d6ee6ab2eb5a7c1d4971115
ichrak03 <78682036+ichrak03@users.noreply.github.com>
2021-03-08 14:53:09 +0100

    vich

---
+++ src/main/java/tn/rnu/isi/worldcup/entities/Match.java
@@ -0,0 +1,77 @@
+package tn.rnu.isi.worldcup.entities;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+@DynamicUpdate
+@Entity
+@Table(name = "match")
+public class Match implements Serializable{
+	private static final long serialVersionUID = 1L;
+		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "matchid")
+		private Long id;
+		private Date date_match;
+		private int nbr_buts;
+		private int heure_match;
+		//constructeur sans parametres
+		public Match() {
+			super();
+		}
+		//constructeur avec tous les parametres
+		public Match(Long id, Date date_match, int nbr_buts, int heure_match) {
+			super();
+			this.id = id;
+			this.date_match = date_match;
+			this.nbr_buts = nbr_buts;
+			this.heure_match = heure_match;
+		}
+
+		//getters et setters
+		public Long getId() {
+			return id;
+		}
+	
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public Date getDate_match() {
+			return date_match;
+		}
+		public void setDate_match(Date date_match) {
+			this.date_match = date_match;
+		}
+		public int getNbr_buts() {
+			return nbr_buts;
+		}
+		public void setNbr_buts(int nbr_buts) {
+			this.nbr_buts = nbr_buts;
+		}
+		public int getHeure_match() {
+			return heure_match;
+		}
+		public void setHeure_match(int heure_match) {
+			this.heure_match = heure_match;
+		}
+		//methode to string affichage de l'objet
+		@Override
+		public String toString() {
+			return "Match [id=" + id + ", date_match=" + date_match + ", nbr_buts=" + nbr_buts + ", heure_match="
+					+ heure_match + "]";
+		}
+		
+		
+		
+		
+}
---
+++ src/test/java/tn/rnu/isi/worldcup/Worldcup2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.rnu.isi.worldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Worldcup2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
