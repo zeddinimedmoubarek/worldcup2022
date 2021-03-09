6b0a0bc5e5bde2cf26a5e2a11d1ff642c7f16dbc
ichrak03 <78682036+ichrak03@users.noreply.github.com>
2021-03-09 19:00:31 +0100

    Update UserController.java

--- src/main/java/tn/rnu/isi/worldcup/controller/UserController.java
+++ src/main/java/tn/rnu/isi/worldcup/controller/UserController.java
@@ -5,37 +5,45 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import tn.rnu.isi.worldcup.config.*;
+import tn.rnu.isi.worldcup.entities.*;
 import tn.rnu.isi.worldcup.entities.User;
-import tn.rnu.isi.worldcup.repository.UserRepository;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 	
 	@Autowired
-	private UserRepository userRepository;
-	
-	//@Secured({"ROLE_ADMIN", "ROLE_USER"})
+    private UserService userService;
+
+    //@Secured({"ROLE_ADMIN", "ROLE_USER"})
     @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value="/users", method = RequestMethod.GET)
-	public List<User> getUsers(){
-		return (List<User>) userRepository.findAll();
-	}
+    public List<User> listUser(){
+        return userService.findAll();
+    }
+
     //@Secured("ROLE_USER")
     @PreAuthorize("hasRole('USER')")
+    ////@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
-	public User getUser(Long id) {
-		return userRepository.findUserById(id);
-	}
-	
-	@PostMapping("/adduser")
-	void addUser(@RequestBody User user) {
-		userRepository.save(user);
-	}
+    public User getOne(@PathVariable(value = "id") Long id){
+        return userService.findById(id);
+    }
+
+
+    @RequestMapping(value="/signup", method = RequestMethod.POST)
+    public User saveUser(@RequestBody UserDto user){
+        return userService.save(user);
+    }
+
+
 }
