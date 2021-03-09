10e33d6ce54a4c1e362bc81563a4d6797bc66528
ichrak03 <78682036+ichrak03@users.noreply.github.com>
2021-03-09 11:29:43 +0100

    Add files via upload

---
+++ src/main/java/tn/rnu/isi/worldcup/entities/Role.java
@@ -0,0 +1,44 @@
+package tn.rnu.isi.worldcup.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/tn/rnu/isi/worldcup/entities/UserDto.java
@@ -0,0 +1,34 @@
+package tn.rnu.isi.worldcup.entities;
+
+public class UserDto {
+	private String username;
+    private String password;
+    private int age;
+   
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+}
